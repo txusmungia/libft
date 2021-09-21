165bffe365953a5365aab612cfb4c37b65626007
Txus Martin <txusmungia@hotmail.com>
2021-09-21 20:09:17 +0200

    empujon

---
+++ ft_itoa.c
@@ -0,0 +1,51 @@
+#include "libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+static size_t	get_int_len(int n)
+{
+	size_t	n_len;
+
+	n_len = 0;
+	if (n <= 0)
+		n_len = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		n_len++;
+	}
+	return (n_len);
+}
+
+static int	abs_val(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+char	*ft_itoa(int n)
+{	
+	char	*str;
+	int		n_len;
+	int		sign;
+
+	n_len = get_int_len(n);
+	str = (char *)malloc(sizeof(char) * (n_len + 1));
+	if (!str)
+		return (0);
+	str[n_len] = 0;
+	sign = 1;
+	if (n < 0)
+		sign = 0;
+	while (--n_len >= 0)
+	{
+		str[n_len] = abs_val(n % 10) + 48;
+		n /= 10;
+	}
+	if (!sign)
+		str[0] = (char)45;
+	return (str);
+}
+
+}
