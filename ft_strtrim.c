01d592a4f7d972e143075b07538228177ac71511
Jesus Martin orbea <jemartin@c3r1s1.42urduliz.com>
2021-09-27 18:01:43 +0200

    bueno

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jemartin <jemartin@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:57:22 by jemartin          #+#    #+#             */
-/*   Updated: 2021/09/26 16:58:43 by jemartin         ###   ########.fr       */
+/*   Updated: 2021/09/27 17:58:05 by jemartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 
-	i = ft_strlen(s1);
 	if (!s1 || !set)
 		return (0);
+	i = ft_strlen(s1);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	i = ft_strlen(s1);
